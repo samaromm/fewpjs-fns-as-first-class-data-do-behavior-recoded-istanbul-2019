542d2ed32f07b1e409dc39e177b303ce0500624c
Samar Omar <samaromm98@gmail.com>
2020-01-22 20:18:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeStr){
+  let timeNum= time.parseInt();
+  if()
+}
 /* Write your implementation of displayMessage() */
